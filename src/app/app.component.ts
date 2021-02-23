4718fe22e94bf1185c0e54dbe3fad426d1ac470d
Trung Doan <duytrung177@gmail.com>
2021-02-23 11:28:04 +0700

    up project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<h1>Welcome to !</h1>\n' +
+    '        <button (click)="increment()">Increment</button>\n' +
+    '        <button (click)="decrement()">decrement</button>\n' +
+    '        <app-child [count]=Counter></app-child>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'input-demo';
+  Counter = 5;
+  increment(){
+    this.Counter++;
+  }
+  decrement(){
+    this.Counter--;
+  }
 }
